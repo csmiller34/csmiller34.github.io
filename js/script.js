6c5fba12aed3a47eac09d9d832252b8b3d4c7ca7
Craig Miller <cm134555@ncr.com>
2019-06-12 14:25:00 -0400

    Updates and fixed mobile menu selection

--- js/script.js
+++ js/script.js
@@ -1,44 +1,46 @@
+/*jshint esversion: 6 */
+
 document.addEventListener("DOMContentLoaded", function() {
    var timer1 = setTimeout(function() {
-      document.querySelectorAll('section').forEach(function(el) {
+      document.querySelectorAll("section").forEach(function(el) {
          el.style.opacity = 1.0;
-      })
+      });
    }, 500);
   checkMenu();
-})
+});
 
-window.addEventListener('resize', function() {
+window.addEventListener("resize", function() {
   checkMenu();
-})
+});
 
 function checkMenu() {
    if (document.body.offsetWidth > 899)  {
-      document.querySelector('nav ul').style.display = "block";
+      document.querySelector("nav ul").style.display = "block";
    } else {
-      document.querySelector('nav ul').style.display = "none";
+      document.querySelector("nav ul").style.display = "none";
    }
 }
 
-document.getElementsByTagName('body')[0].onscroll = function() {
+document.getElementsByTagName("body")[0].onscroll = function() {
    var scroller = document.documentElement.scrollTop;
    if (scroller > 100) {
-      document.querySelector('.go-to-top').classList.remove('go-to-top-hidden');
-      document.querySelector('.go-to-top-arrow').classList.remove('go-to-top-arrow-hidden');
+      document.querySelector(".go-to-top").classList.remove("go-to-top-hidden");
+      document.querySelector(".go-to-top-arrow").classList.remove("go-to-top-arrow-hidden");
    } else {
-      document.querySelector('.go-to-top').classList.add('go-to-top-hidden');
-      document.querySelector('.go-to-top-arrow').classList.add('go-to-top-before-hidden');
+      document.querySelector(".go-to-top").classList.add("go-to-top-hidden");
+      document.querySelector(".go-to-top-arrow").classList.add("go-to-top-before-hidden");
    }
-}
+};
 
 var myposition;
 var timer;
-document.querySelector('.go-to-top').addEventListener('click', scroller);
+document.querySelector(".go-to-top").addEventListener("click", scroller);
 
 function scroller() {
    myposition = document.documentElement.scrollTop;
    myposition -= 200;
    if ( myposition > 0 ) {
-   document.documentElement.scrollTop = myposition
+   document.documentElement.scrollTop = myposition;
    timer = setTimeout(scroller, 20);
    } else {
       document.documentElement.scrollTop = 0;
@@ -52,84 +54,87 @@ var currentIndent = {"scroller1" : 0, "scroller2" : 0, "scroller3" : 0, "persona
 var indentSize;
 function leftArrow(that) {
   var root = that.nextElementSibling;
-  indentSize = root.querySelector('img').clientWidth + 10;
+  indentSize = root.querySelector("img").clientWidth + 10;
   if (currentIndent[that.parentElement.getAttribute("id")] < 0 ) {
     currentIndent[that.parentElement.getAttribute("id")] += indentSize;
   }
   if (currentIndent[that.parentElement.getAttribute("id")] >= 0) {
-    showHideArrows(root.parentElement, '.arrow-left', 'hide')
+    showHideArrows(root.parentElement, ".arrow-left", "hide");
   }
-  root.style.textIndent = currentIndent[that.parentElement.getAttribute("id")] + 'px';
-  showHideArrows(root.parentElement, '.arrow-right', 'show')
+  root.style.textIndent = currentIndent[that.parentElement.getAttribute("id")] + "px";
+  showHideArrows(root.parentElement, ".arrow-right", "show");
 }
 function rightArrow(that) {
   var root = that.previousElementSibling;
-  indentSize = root.querySelector('img').clientWidth + 10;
+  indentSize = root.querySelector("img").clientWidth + 10;
   if (root.scrollWidth > root.clientWidth ) {
     currentIndent[that.parentElement.getAttribute("id")] -= indentSize;
-    root.style.textIndent = currentIndent[that.parentElement.getAttribute("id")] + 'px';
+    root.style.textIndent = currentIndent[that.parentElement.getAttribute("id")] + "px";
   }
   if ((root.scrollWidth - root.clientWidth) < indentSize ) {
-      showHideArrows(root.parentElement, '.arrow-right', 'hide')
+      showHideArrows(root.parentElement, ".arrow-right", "hide");
   }
-  showHideArrows(root.parentElement, '.arrow-left', 'show')
+  showHideArrows(root.parentElement, ".arrow-left", "show");
 }
 function showHideArrows(root, whichArrow, whichDirection) {
   if (whichDirection === "show") {
       root.querySelector(whichArrow).style.display = "block";
-      root.querySelector(whichArrow).classList.remove('hide-element');
+      root.querySelector(whichArrow).classList.remove("hide-element");
   } else { 
-    root.querySelector(whichArrow).classList.add('hide-element');
+    root.querySelector(whichArrow).classList.add("hide-element");
     var timer = setTimeout(function() {
       root.querySelector(whichArrow).style.display = "none";
     }, 250);
   }
 }
 
-document.querySelectorAll('.siema').forEach( function(elem, index) {
+document.querySelectorAll(".siema").forEach( function(elem, index) {
    var whichSiema = "#siema" + index;
    var mySiema = new Siema({ selector: whichSiema, onChange: updateArrows  });
-   elem.parentElement.querySelector('.prev').addEventListener('click', () => mySiema.prev()); 
-   elem.parentElement.querySelector('.next').addEventListener('click', () => mySiema.next());
-
-//   elem.parentElement.querySelectorAll('.siema-btn').forEach(function(e) {
-//      e.style.marginTop = "-" + parseInt(elem.parentElement.clientHeight / 2) + "px";
-//      e.parentElement.querySelector('.siema-btn.next').style.left = parseInt(elem.parentElement.clientWidth + 20) + "px";
-//   })
-})
-
-function updateArrows() {
-   var this_next = this.selector.parentElement.querySelector('.next');
-   var this_prev  = this.selector.parentElement.querySelector('.prev');
-   if (this.currentSlide < (this.selector.parentElement.querySelector('.siema > div').childElementCount)-1) {
-      this_next.classList.add('siema-active');
-   } else {
-      this_next.classList.remove('siema-active');
-   }
-   if (this.currentSlide > 0) {
-      this_prev.classList.add('siema-active');
-   } else {
-      this_prev.classList.remove('siema-active');
-   }
-   this.selector.parentElement.querySelector('.current-page').innerHTML = this.currentSlide + 1;
-}
-
-document.querySelectorAll('.lightbox').forEach( function(elem) { new Luminous(elem); });
-var galleryOpts = { arrowNavigation: true, closeWithEscape: true };
-
-new LuminousGallery(document.querySelectorAll('.lightbox-gallery'), galleryOpts, { 
-   caption: function (trigger) { return trigger.querySelector('img').getAttribute('alt');  } 
+   elem.parentElement.querySelector(".prev").addEventListener("click", () => mySiema.prev()); 
+   elem.parentElement.querySelector(".next").addEventListener("click", () => mySiema.next());
 });
 
-document.querySelector('.mobile-menu').addEventListener('click', showHideMenu);
-document.querySelector('.mobile-menu').addEventListener('blur', showHideMenu);
+function updateArrows() {
+   var this_next = this.selector.parentElement.querySelector(".next");
+   var this_prev  = this.selector.parentElement.querySelector(".prev");
+   if (this.currentSlide < (this.selector.parentElement.querySelector(".siema > div").childElementCount)-1) {
+      this_next.classList.add("siema-active");
+   } else {
+      this_next.classList.remove("siema-active");
+   }
+   if (this.currentSlide > 0) {
+      this_prev.classList.add("siema-active");
+   } else {
+      this_prev.classList.remove("siema-active");
+   }
+   this.selector.parentElement.querySelector(".current-page").innerHTML = this.currentSlide + 1;
+}
+
+document.querySelectorAll(".lightbox").forEach( function(elem) { new Luminous(elem); });
+var galleryOpts = { arrowNavigation: true, closeWithEscape: true };
+
+new LuminousGallery(document.querySelectorAll(".lightbox-gallery"), galleryOpts, { 
+   caption: function (trigger) { return trigger.querySelector("img").getAttribute("alt");  } 
+});
+
+document.querySelector(".mobile-menu").addEventListener("click", showHideMenu);
+document.querySelector(".mobile-menu").addEventListener("blur", showHideMenu);
 
 function showHideMenu() {
-   console.log("test")
-   var showHide = document.querySelector('nav ul');
-   if ((showHide.style.display === 'none') || (showHide.style.display === '')) {
+   var showHide = document.querySelector("nav ul");
+   if ((showHide.style.display === "none") || (showHide.style.display === "")) {
       showHide.style.display = "block";
    } else {
       showHide.style.display = "none";
    }
+}
+
+document.querySelectorAll("nav > ul > li > a").forEach( elem => {
+   elem.addEventListener("mouseup", chooseMobileMenu);
+   elem.addEventListener("touch", chooseMobileMenu);
+});
+
+function chooseMobileMenu() {
+   window.location.href = this.getAttribute("href");
 }
